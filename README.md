8cfe5f8a7f8081ac331deae1d17aee4c43565493
Katie Li <katiesli16@gmail.com>
2015-05-21 21:13:19 -0600

    initial website from Django tutorial 1-4

---
+++ README.md
@@ -0,0 +1,2 @@
+the following command is necessary to add on subclasses of those choices
+<q.choice_set.create() >
---
+++ polls/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from polls.models import Question, Choice
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+  def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name ='polls/detail.html'
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name ='polls/results.html'
+
+
+def vote(request, pk):
+  p = get_object_or_404(Question, pk=pk)
+  try: 
+    selected_choice = p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question' : p,
+      'error_message': "You didn't select a choice.",
+    })
+  else:
+    selected_choice.votes +=1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+#####################################################
+#### functions below not necessarily used
+# Create your views here.
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5] 
+  ## a json :) output ayaya
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id) 
+  return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id): 
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
+
